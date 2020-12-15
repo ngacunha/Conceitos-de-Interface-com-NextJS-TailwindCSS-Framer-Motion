50c5316f072b05ab881e36aace7a1670f86ed47f
Nelson Cunha <ngabrieel@hotmail.com>
2020-12-15 19:56:00 -0300

    first commit

---
+++ components/Layout.tsx
@@ -0,0 +1,12 @@
+import { Children } from "react"
+import Header from "./Header"
+
+const Layout: React.FC = ({children}) => {
+  return (
+  <div className="max-w-5xl mx-auto px-4 md:px-8 sm:px-6">
+    <Header />
+    {children}
+    </div>
+  )
+}
+export default Layout
